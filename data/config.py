7e9ab743347bd2340475e698f2252a41717a109c
BisratM <mogesbisrat@gmail.com>
2020-01-11 05:17:47 -0500

    added cityscapes dataset

--- data/config.py
+++ data/config.py
@@ -172,6 +172,24 @@ pascal_sbd_dataset = dataset_base.copy({
     'class_names': PASCAL_CLASSES,
 })
 
+CITYSCAPES_CLASSES = ("road","sidewalk","parking","rail","track","person", "rider",
+                      "car", "truck", "bus" ,"on rails" ,"motorcycle","bicycle", "caravantrailer",
+                      "building", "wall", "fence", "guard", "rail",  "bridge",  "tunnel",
+                      "pole",  "pole", "group",  "traffic sign",  "traffic light",
+                      "vegetation",  "terrain", "sky" , "ground", "dynamic", "static")
+
+cityscapes_dataset = dataset_base.copy({
+    'name': 'cityscapes',
+
+    'train_images': './data/coco/images/train',
+    'train_info': './data/coco/annotations/instances_train2017.json',
+    # Validation images and annotations.
+    'valid_images': './data/coco/images/val',
+    'valid_info': './data/coco/annotations/instances_val2017.json',
+
+    'class_names': CITYSCAPES_CLASSES,
+})
+
 
 
 
@@ -678,6 +696,64 @@ yolact_base_config = coco_base_config.copy({
         'pred_scales': [[24], [48], [96], [192], [384]],
     }),
 
+    # FPN Settings
+    'fpn': fpn_base.copy({
+        'use_conv_downsample': True,
+        'num_downsample': 2,
+    }),
+
+    # Mask Settings
+    'mask_type': mask_type.lincomb,
+    'mask_alpha': 6.125,
+    'mask_proto_src': 0,
+    'mask_proto_net': [(256, 3, {'padding': 1})] * 3 + [(None, -2, {}), (256, 3, {'padding': 1})] + [(32, 1, {})],
+    'mask_proto_normalize_emulate_roi_pooling': True,
+
+    # Other stuff
+    'share_prediction_module': True,
+    'extra_head_net': [(256, 3, {'padding': 1})],
+
+    'positive_iou_threshold': 0.5,
+    'negative_iou_threshold': 0.4,
+
+    'crowd_iou_threshold': 0.7,
+
+    'use_semantic_segmentation_loss': True,
+})
+
+cityscapes_config = coco_base_config.copy({
+    'name': 'cityscapes_config',
+
+    # Dataset stuff
+    'dataset': cityscapes_dataset,
+    'num_classes': len(cityscapes_dataset.class_names) + 1,
+
+    # Image Size
+    'max_size': 550,
+
+    # Training params
+    'lr_steps': (280000, 600000, 700000, 750000),
+    'max_iter': 800000,
+
+    # Backbone Settings
+    # 'backbone': resnet101_backbone.copy({
+    #     'selected_layers': list(range(1, 4)),
+    #     'use_pixel_scales': True,
+    #     'preapply_sqrt': False,
+    #     'use_square_anchors': True,  # This is for backward compatability with a bug
+    #
+    #     'pred_aspect_ratios': [[[1, 1 / 2, 2]]] * 5,
+    #     'pred_scales': [[24], [48], [96], [192], [384]],
+    # }),
+    'backbone': resnet50_backbone.copy({
+        'selected_layers': list(range(1, 4)),
+
+        'pred_scales': yolact_base_config.backbone.pred_scales,
+        'pred_aspect_ratios': yolact_base_config.backbone.pred_aspect_ratios,
+        'use_pixel_scales': True,
+        'preapply_sqrt': False,
+        'use_square_anchors': True,  # This is for backward compatability with a bug
+    }),
     # FPN Settings
     'fpn': fpn_base.copy({
         'use_conv_downsample': True,
